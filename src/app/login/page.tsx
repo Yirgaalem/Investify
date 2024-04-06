771adbfec1d8a928638e44372f734981d28dbd4a
Nahor Yirgaalem <nahor21@hotmail.com>
2024-04-06 02:06:19 -0400

    finished sign up tab, finalizing logo and back to home button, maybe remove arrow

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,86 +3,85 @@ import { useState } from 'react';
 import "./login.css";
 import Image from "next/image";
 import google from '../../../public/icons/google-icon.svg';
+import arrow from "../../../public/images/arrow.png";
 
 export default function Login() {
 
-  /*const container: HTMLElement|null = document.getElementById('container');
-  const registerButton: HTMLElement|null = document.getElementById('register');
-  const loginButton: HTMLElement|null = document.getElementById('login');
+  const [isActive, setIsActive] = useState<Boolean>(false);
+  let headerText: string = isActive ? "Let's Go" : 'Get Started';
 
-  if (container) {
-    if (registerButton) {
-      registerButton.addEventListener('click', () => {
-        container.classList.add("active");
-      });
-    }
-    if (loginButton) {
-      loginButton.addEventListener('click', () => {
-          container.classList.remove("active");
-      });
-    }
-  } */
-  const [isActive, setIsActive] = useState(false);
   return (
     <>
-      <div className={`container ${isActive ? 'active':''}`} id="container">
-        <div className="form-container sign-up">
-            <form>
-                <h1>Create Account</h1>
-                <div className="social-icons">
-                    <a href="#" className="icon">
-                      <Image className="google" src={google} alt='Google Image'/>
-                    </a>
-                </div>
-                
-                <span>or use your email for registeration</span>
-                <input type="text" placeholder="Name"/>
-                <input type="email" placeholder="Email"/>
-                <input type="password" placeholder="Password"/>
+      <div className="headerContainer">
+        <div className="investify">Investify</div>
+        <div className="home"><button>Home</button><Image className="arrow" src={arrow} alt='arrow'/></div>
+      </div>
 
-                <button>Sign Up</button>
-            </form>
-        </div>
-        
-        <div className="form-container sign-in">
-            <form>
-                <div className="signInHeader">Sign In</div>
-                <input type="email" placeholder="Email"/>
-                <input type="password" placeholder="Password"/>
-                <a href="#">Forget Your Password?</a>
-                
-                <button className="signInButton">Sign In</button>
-                
-                <div className="line"></div>
-                <div className="social-icons">
-                    <a href="#" className="icon">
-                      <Image className="google" src={google} alt='Google Image'/>
-                    </a>
-                    Log in with Google
-                </div>
-            </form>
-        </div>
+      <div className="tabContainer">
+        <div className="headerText">{headerText}</div>
 
-        <div className="toggle-container">
-            <div className="toggle">
-                
-                <div className="toggle-panel toggle-left">
-                    <h1>Have an account?</h1>
-                    <p>Login and continue managing your investments</p>
-                    <button id="login" onClick={() => {setIsActive(!isActive)}}>Sign In</button>{/*both this and the reigster sign up button className="hidden"*/} 
-                </div>
+        <div className={`container ${isActive ? 'active':''}`} id="container">
+          <div className="form-container sign-up">
+              <form>
+                  <div className="createAccountHeader">Create Account</div>
+                  
+                  {/* <span>or use your email for registeration</span> */}
+                  <input type="text" placeholder="Username"/>
+                  <input type="email" placeholder="Email"/>
+                  <input type="password" placeholder="Password"/>
 
-                <div className="toggle-panel toggle-right">
-                    <div className="welcome">Welcome!</div>
-                    <p>Sign up today and watch your investments take flight!</p>
-                    <button id="register" onClick={() => {setIsActive(!isActive)}}>Sign Up</button>
-                </div>
-            
-            </div>
-        </div>
-    </div>
+                  <button>Sign Up</button>
+
+                  <div className='line'></div>
+                  <div className="social-icons">
+                      <a href="#" className="icon">
+                        <Image className="google" src={google} alt='Google Image'/>
+                      </a>
+                      Sign in with Google
+                  </div>
+
+              </form>
+          </div>
+          
+          <div className="form-container sign-in">
+              <form>
+                  <div className="signInHeader">Sign In</div>
+                  <input type="email" placeholder="Email"/>
+                  <input type="password" placeholder="Password"/>
+                  <a href="#">Forget Your Password?</a>
+                  
+                  <button className="signInButton">Sign In</button>
+                  
+                  <div className="line"></div>
+                  <div className="social-icons">
+                      <a href="#" className="icon">
+                        <Image className="google" src={google} alt='Google Image'/>
+                      </a>
+                      Log in with Google
+                  </div>
+              </form>
+          </div>
+
+          <div className="toggle-container">
+              <div className="toggle">
+                  
+                  <div className="toggle-panel toggle-left">
+                      <div className="account">Have an account?</div>
+                      <p>Login and continue managing your investments</p>
+                      <button id="login" onClick={() => {setIsActive(!isActive)}}>Sign In</button>{/*both this and the reigster sign up button className="hidden"*/} 
+                  </div>
+
+                  <div className="toggle-panel toggle-right">
+                      <div className="welcome">Welcome!</div>
+                      <p>Sign up today and watch your investments take flight!</p>
+                      <button id="register" onClick={() => {setIsActive(!isActive)}}>Sign Up</button>
+                  </div>
+              
+              </div>
+          </div>
+      </div>
+      </div>
     </>
-
   );
   
 }
