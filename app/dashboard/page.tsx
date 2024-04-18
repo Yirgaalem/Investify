f8714d67f1d6c142d1f0bfffa32276d198bb610c
Nahor Yirgaalem <nahor21@hotmail.com>
2024-04-18 14:19:31 -0400

    fixed header position

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,10 +9,9 @@ import Charts from '../components/dashboardComponents/charts/page';
 export default function Dashboard() {
   
   return (
-    <>
+    <PageLayout>
       <Header headerLeft='Portfolio' headerMiddle='Dashboard' headerRight='Charts'/>
       
-      <PageLayout>
         <div className={styles.dashboardContent}>
           <div className={styles.top}>
             <Total amount={103030.90} pnl={28019.90}/>
@@ -24,7 +23,6 @@ export default function Dashboard() {
             <Charts/>
           </div>
         </div>
-      </PageLayout>
-    </>
+    </PageLayout>
   );
 }
